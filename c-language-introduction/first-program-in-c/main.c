6eac8d4f6286abf956a2dae5af13303a0f6fe831
Diego Gonzalez <diegomzalez@hotmail.com>
2024-08-20 10:32:18 -0600

    feat: Understanding the history, features and low basics of C.

---
+++ c-language-introduction/first-program-in-c/main.c
@@ -0,0 +1,14 @@
+// Header Section
+#include <stdio.h>
+
+// Main Section
+int main(void)
+{ // Function body starts from here.
+    // Declaration and initialization of an in variable with a value of 10.
+    int a = 10;
+
+    // Instruction to print the value of a variable in the terminal.
+    printf("%d", a);
+
+    return 0;
+} // Function body ends here.
